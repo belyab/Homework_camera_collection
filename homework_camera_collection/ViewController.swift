f7ad523880bc2e77bab92b06428a3a227e6c0df4
Ilyas Ikhsanov <ilyas.ikhsanov@x5.ru>
2022-04-29 18:48:17 +0300

    first commit

---
+++ homework_camera_collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_camera_collection
+//
+//  Created by ilyas.ikhsanov on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
